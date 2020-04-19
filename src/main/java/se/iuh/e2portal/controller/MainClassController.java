af3e8587e66d5651b3004d7b6f5026f999f64a56
minhlk320 <truongquocminh.it@gmail.com>
2020-04-19 13:46:02 +0700

    Change to Vietnamese, add view lecturer

---
+++ src/main/java/se/iuh/e2portal/controller/MainClassController.java
@@ -0,0 +1,23 @@
+package se.iuh.e2portal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import se.iuh.e2portal.service.MainClassService;
+import se.iuh.e2portal.service.ModuleClassService;
+
+@Controller
+@RequestMapping("/mainclass")
+public class MainClassController {
+    @Autowired
+    MainClassService mainClassService;
+    @GetMapping("")
+    public String getMainClass(@PageableDefault(size = 10) Pageable pageable, Model model) {
+        model.addAttribute("listClass",mainClassService.findAll());
+        return "mainclass";
+    }
+}
